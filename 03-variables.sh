6ecb606137c129aff510f53a03ec6f9b0279361c
sudha <Sudhaas2802@gmail.com>
2024-12-30 17:33:02 -0500

    variable3

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=aadya
+PERSON2=siddhu
+
+echo "$PERSON1::hi $PERSON2"
+echo "$PERSON2::hello $PERSON1"
+echo "$PERSON1::how are you doing?"
+echo "$PERSON2::i am good.how are you?"
